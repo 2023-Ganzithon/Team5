c1c7b5827986289e72b18f8b6956e97fda9568f0
YouSooA <kinsk2839@naver.com>
2023-11-03 20:18:29 +0900

    feat: PATH 상수 생성
    - Route path 설정할 때 사용할 객체 상수 생성

---
+++ fe/src/constants/path.js
@@ -0,0 +1,15 @@
+export const PATH = {
+  HOME: '/',
+  DONATION: '/donation',
+  REVIEW_HOME: '/reviewHome',
+  REVIEW_WRITE: '/reviewWrite',
+  GAIN_POINT: '/gainPoint',
+  REVIEW_DETAIL: '/reviewDetail',
+  MALL_LIST: '/mallList',
+  MY_PAGE: '/myPage',
+  POINT_HISTORY: '/pointHistory',
+  DONATION_HISTORY: '/donationHistory',
+  DONATION_REGISTRATION: '/donationRegistration',
+  MAP: '/map',
+  LOGIN: '/login',
+};
