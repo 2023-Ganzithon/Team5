e4c85f9c16438d5add88c50bf46b36dccd76926b
YouSooA <kinsk2839@naver.com>
2023-11-03 20:20:41 +0900

    feat: Route 설정

--- fe/src/index.jsx
+++ fe/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { worker } from 'mocks/browsers';
+import App from 'App';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
@@ -11,7 +12,9 @@ if (process.env.NODE_ENV === 'development') {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
