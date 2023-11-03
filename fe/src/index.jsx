5188d0c0f94d6733512e8762c1c641224b844370
YouSooA <kinsk2839@naver.com>
2023-11-03 19:02:10 +0900

    chore: msw 초기 설정

--- fe/src/index.jsx
+++ fe/src/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { worker } from 'mocks/browsers';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
