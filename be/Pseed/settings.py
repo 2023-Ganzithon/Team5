4a8c95da8cba22362262301c55377bf8d2a10321
woorimj <128919405+woorimj@users.noreply.github.com>
2023-11-06 18:23:41 +0900

    공원데이터 model 저장

--- be/Pseed/settings.py
+++ be/Pseed/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     
     "user",
     "myPage",
+    "map",
 ]
 
 MIDDLEWARE = [
--- be/map/models.py
+++ be/map/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Park(models.Model):
+    name = models.CharField(verbose_name="장소이름", primary_key=True, max_length=50)
+    add = models.TextField(verbose_name="주소")
+
+    def __str__(self):
+        return self.name
+    
+
