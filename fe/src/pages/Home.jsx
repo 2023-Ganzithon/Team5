9786bb98e48814e65fe0d2a0bc7fbbdc438f216a
kye1115z <kye1115z@naver.com>
2023-11-07 05:06:13 +0900

    style: Home 페이지 테스트 코드 삭제

--- fe/src/pages/Home.jsx
+++ fe/src/pages/Home.jsx
@@ -10,42 +10,7 @@ const Home = () => {
 
   return (
       <>
-      <div>
-        <Button
-          text = "리뷰 작성하기"
-          path = {PATH.REVIEW_WRITE}
-        ></Button>
-      </div>
 
-      <div>
-        <Button
-          text = "홈 화면으로"
-          path = {PATH.HOME}
-        ></Button>
-      </div>
-
-      <div>
-        <Button
-          text = "쇼핑몰로 이동하기"
-          path = {MALLNAME.MARCO}
-        ></Button>
-      </div>
-
-      <div>
-        <Button
-          text = "기부처 등록하기"
-          path = {PATH.DONATION_REGISTRATION}
-        ></Button>
-      </div>
-
-
-      <div>
-        <Button
-          text = "등록하기"
-          path = {PATH.MY_PAGE}
-          eventName = {handleSubmit}
-        ></Button>
-      </div>
       </>
     )
 };
