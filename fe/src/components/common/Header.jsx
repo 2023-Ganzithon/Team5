f71701c1c81c901d66921a50380e1827eb3a0fd3
kye1115z <kye1115z@naver.com>
2023-11-08 03:56:44 +0900

    style: prettier 적용

--- fe/src/components/common/Header.jsx
+++ fe/src/components/common/Header.jsx
@@ -6,35 +6,38 @@ import Icon from './Icon';
 import COLOR from '@styles/color';
 import FONT from '@styles/fonts';
 
-const Header = ({title, backUrl})=>{
-    const navigate = useNavigate();
+const Header = ({ title, backUrl }) => {
+  const navigate = useNavigate();
 
-    return (<HeadContainer>
-        <Icon 
+  return (
+    <HeadContainer>
+      <Icon
         name="leftArrow"
         iconColor={COLOR.green500}
         onClick={() => navigate(backUrl)}
         style={{
           position: 'absolute',
           left: '7px',
-        }}/>
-        <p>{title}</p>
-        </HeadContainer>);
-}
+        }}
+      />
+      <p>{title}</p>
+    </HeadContainer>
+  );
+};
 
 export default Header;
 
 const HeadContainer = styled.div`
-position: sticky;
-top: 0;
-z-index: 10;
-display: flex;
-justify-content: center;
-align-items: center;
-height: 55px;
-background-color: ${COLOR.white};
-border-bottom: 7px solid ${COLOR.gray200};
-p{
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 55px;
+  background-color: ${COLOR.white};
+  border-bottom: 7px solid ${COLOR.gray200};
+  p {
     ${FONT.title2}
-}
-`
+  }
+`;
--- fe/src/components/common/LogoHeader.jsx
+++ fe/src/components/common/LogoHeader.jsx
@@ -4,29 +4,31 @@ import styled from 'styled-components';
 
 import COLOR from '@styles/color';
 import FONT from '@styles/fonts';
-import LOGO from '@assets/LogoText.png'
+import LOGO from '@assets/LogoText.png';
 
-const LogoHeader = ({title, backUrl})=>{
-    const navigate = useNavigate();
+const LogoHeader = ({ title, backUrl }) => {
+  const navigate = useNavigate();
 
-    return (<HeadContainer>
-        <img src={LOGO}/>
-        </HeadContainer>);
-}
+  return (
+    <HeadContainer>
+      <img src={LOGO} />
+    </HeadContainer>
+  );
+};
 
 export default LogoHeader;
 
 const HeadContainer = styled.div`
-position: sticky;
-top: 0;
-z-index: 10;
-display: flex;
-justify-content: center;
-align-items: center;
-height: 55px;
-background-color: ${COLOR.white};
-border-bottom: 7px solid ${COLOR.gray200};
-img{
-    width:100px;
-}
-`
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 55px;
+  background-color: ${COLOR.white};
+  border-bottom: 7px solid ${COLOR.gray200};
+  img {
+    width: 100px;
+  }
+`;
