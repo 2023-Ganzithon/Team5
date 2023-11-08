21b04c84af4e293b2163debe0cc89242c4fd1670
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-11-08 16:41:27 +0900

    fix: posts.url 수정

--- be/posts/urls.py
+++ be/posts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework import routers
 from .views import PostViewSet
 
-router = routers.SimpleRouter()
-router.register("posts", PostViewSet)
+# 라우터 사용
+router = routers.SimpleRouter()  # SimpleRouter 객체를 생성
+router.register("", PostViewSet)  # register 메서드를 사용해서 viewset을 등록
 
 urlpatterns = router.urls
