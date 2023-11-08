904c128a7cab7e48aaf2051822d8ab2a371c6415
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-11-08 21:24:41 +0900

    feat: point 및 redirect 주석처리

--- be/posts/views.py
+++ be/posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer, PostCreateSerializer
 from .permissions import CustomReadOnly
@@ -10,16 +11,23 @@ from django_filters.rest_framework import DjangoFilterBackend
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [CustomReadOnly]
-
-    # shoppingmall, star filtering
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["shoppingmall", "star"]
 
     def get_serializer_class(self):
-        if self.action == "list" or "retrieve":
+        if self.action in ["list", "retrieve"]:
             return PostSerializer
         return PostCreateSerializer
 
     def perform_create(self, serializer):
+        # Get the current user's profile
         profile = Profile.objects.get(user=self.request.user)
+
+        # Set the author and profile for the post
         serializer.save(author=self.request.user, profile=profile)
+
+        # point 가산 로직
+        # profile.point += 10
+        # profile.save()
+
+        # return redirect('')
