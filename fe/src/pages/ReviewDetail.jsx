2706d8b27e891be45905dd45a6d007d9da171ddd
kye1115z <kye1115z@naver.com>
2023-11-09 02:05:37 +0900

    feat: ReviewDetail id 값 넘겨 받기

--- fe/src/pages/ReviewDetail.jsx
+++ fe/src/pages/ReviewDetail.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ReviewDetail = () => {
+  const { state } = useLocation();
+  console.log(state);
+
   return <div>ReviewDetail</div>;
 };
 
