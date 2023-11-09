12aa5e98c07246311bc41ec66327a7f96239c20f
kye1115z <kye1115z@naver.com>
2023-11-09 13:05:54 +0900

    feat: Rate stories 작성

---
+++ fe/src/stories/Rate.stories.js
@@ -0,0 +1,33 @@
+import Rate from '@components/Rate';
+
+const meta = {
+  title: 'Rate',
+  component: Rate,
+  args: {
+    size: 8,
+    rate: undefined,
+  },
+  argTypes: {
+    size: {
+      description: '아이콘의 크기 설정 integer ex: 8',
+      control: {
+        type: 'number',
+      },
+    },
+    rate: {
+      description: '별점을 설정하고자 한다면 빈칸, 설정된 별점 값이 있다면 0~4 사이의 값으로 전달',
+      control: {
+        type: 'number',
+      },
+    },
+    required: false,
+  },
+};
+export default meta;
+
+const Template = (args) => <Rate {...args}></Rate>;
+export const Primary = Template.bind({});
+Primary.args = {
+  size: 8,
+  rate: 3,
+};
