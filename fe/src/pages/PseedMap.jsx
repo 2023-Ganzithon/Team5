2ba2c54d1499cac683c7fef680a6832b08420bc3
jwkim24 <kjw5841@naver.com>
2023-11-09 20:10:05 +0900

    feat : PseedMap 구현

--- fe/src/pages/PseedMap.jsx
+++ fe/src/pages/PseedMap.jsx
@@ -4,19 +4,39 @@ import Modal from '@components/Modal';
 import LogoHeader from '@common/LogoHeader';
 import COLOR from '@styles/color';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import MarkerImage from '@assets/Pin.png'
+import TabBar from '@common/TabBar';
 
 
 const PseedMap = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const modalBackground = useRef();
-  const [location, setLoacation] = useState(null); // 현재 위치를 저장할 상태
+  const [location, setLoacation] = useState({ lat: 37.4024064, lng: 127.101385 });
+  const [mapMarkers, setMapMarkers] = useState([]);
 
 	useEffect(() => {
-		navigator.geolocation.getCurrentPosition(successHandler, errorHandler); // 성공시 successHandler, 실패시 errorHandler 함수가 실행된다.
+		navigator.geolocation.getCurrentPosition(successHandler, errorHandler); 
 	}, []);
 
+	useEffect(() => {
+		const newMarker = (
+			<MapMarker
+			  key={`current-location`}
+			  position={location}
+			  image={{
+				src: MarkerImage,
+				size: { width: 30, height: 35 },
+			  }}
+			  title={'현재 위치'}
+			/>
+		  );
+		  // 이전 마커 목록과 새로운 마커를 합쳐서 업데이트
+		  setMapMarkers((prevMarkers) => [...prevMarkers, newMarker]);
+	}, [location]);
+	
+
   const successHandler = (response) => {
-		console.log(response); // coords: GeolocationCoordinates {latitude: 위도, longitude: 경도, …} timestamp: 1673446873903
+		console.log(response); 
 		const { latitude, longitude } = response.coords;
 		setLoacation({ latitude, longitude });
 	};
@@ -24,34 +44,33 @@ const PseedMap = () => {
 	const errorHandler = (error) => {
 		console.log(error);
 	};
-
-  const locations = [
-		{ title: '구름스퀘어', latlng: { lat: 37.4024064, lng: 127.101385 } },
-		{ title: '생태연못', latlng: { lat: 33.450936, lng: 126.569477 } },
-		{ title: '텃밭', latlng: { lat: 33.450879, lng: 126.56994 } },
-		{ title: '근린공원', latlng: { lat: 33.451393, lng: 126.570738 } },
-	];
   
-  return (<MapContainer>
+  return (<>
+  <MapContainer>
   <LogoHeader/>
   <KakaoMap
-  center={{ lat: 37.4024064, lng: 127.101385 }}   // 지도의 중심 좌표
+  center={{ lat: location.lat, lng: location.lng }}   // 지도의 중심 좌표
   level={3} >
-    {locations.map((loc, idx) => (
-				<MapMarker
-					key={`${loc.title}-${loc.latlng}`}
-					position={loc.latlng}
-					image={{
-						src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
-						size: { width: 24, height: 35 },
-					}}
-					title={loc.title}
-				/>
-			))}
+	<MapMarker
+	key={`current-location`}
+    position={location}
+    image={{
+        src: MarkerImage,
+        size: { width: 30, height: 35 },
+    }}
+	title={'현재 위치'}/>
+	{mapMarkers}
+	{modalOpen && (
+        <Modal
+          place="장소명" // 모달에 표시할 정보 설정
+          point={10}
+          totalPoint={100}
+          onClose={() => setModalOpen(false)} // 모달 닫기
+        />
+      )}
   </KakaoMap>
-  {/* <Modal place={"오미자"} point={10} totalPoint={100} />
-  <div onClick={()=> setModalOpen(true)}>Map</div> */}
-  </MapContainer>);
+  </MapContainer>
+  <TabBar currentTab='map'/></>);
 };
 
 export default PseedMap;
@@ -63,6 +82,6 @@ height:100%;
 
 const KakaoMap = styled(Map)`
 width:100%;
-min-height: calc(100vh - 55px);
+min-height: calc(100vh - 50px);
 background-color:${COLOR.green500}
 `
