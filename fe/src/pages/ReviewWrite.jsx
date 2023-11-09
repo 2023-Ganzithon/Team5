d160fd98e913b146461c1914902af6b2a11e891e
kye1115z <kye1115z@naver.com>
2023-11-09 13:52:18 +0900

    feat: Dropdown 추가

--- fe/src/pages/ReviewWrite.jsx
+++ fe/src/pages/ReviewWrite.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import Rate from '@components/Rate';
+import Dropdown from '@components/Dropdown';
 
 const ReviewWrite = () => {
   // 설정된 rate 점수
   const [score, setScore] = useState(0);
   const updateScore = (newScore) => {
-    setScore(newScore); // updateScore 함수를 사용하여 score 업데이트
+    setScore(newScore);
+  };
+
+  // dropdown 선택된 값
+  const [select, setSelect] = useState('');
+  const handleSelect = (newSelect) => {
+    setSelect(newSelect);
   };
 
   return (
     <div>
+      <Dropdown updateSelect={handleSelect} />
       <Rate size={8} updateScore={updateScore} />
     </div>
   );
