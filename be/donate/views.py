79cd9cb7c2568e774004458881fd06cc5cb9ced4
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-11-09 17:10:19 +0900

    fix: donate register

--- be/donate/views.py
+++ be/donate/views.py
@@ -6,14 +6,16 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from myPage.models import Donation
+from users.models import Profile
 
 
 class DonatePostView(APIView):
     def post(self, request):
         serializer = DonatePostSerializer(data=request.data)
+        profile = Profile.objects.get(user=self.request.user)
 
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(donator=self.request.user, profile=profile)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
