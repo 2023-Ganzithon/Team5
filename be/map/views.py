4f08a6275eb55efae2a42c97a6b5e32a3072ff8a
woorimj <128919405+woorimj@users.noreply.github.com>
2023-11-10 03:47:32 +0900

    feat: park새로운 모델 db저장, 로직 구현

--- be/map/views.py
+++ be/map/views.py
@@ -7,11 +7,13 @@ from .models import Park
 from myPage.models import Profile
 from myPage.models import ParkVisitPoint
 from datetime import datetime
+
+from django.http import JsonResponse
+from django.views import View
+
+from math import radians, sin, cos, sqrt, atan2
 import json
 
-# Create your views here.
-# def kakaoMap(request):
-#     return render(request, 'map.html')
 
 class EarnParkPointsView(APIView):
     def post(self, request):
@@ -41,5 +43,42 @@ class EarnParkPointsView(APIView):
             return Response({'message': '로그인 하세요.'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
+# 지도포인트 획득 로직 다시한번..생각해보기
+# class FindNearbyParksView(View):
+#     def post(self, request):
+#         user_latitude = radians(float(request.POST.get('latitude')))
+#         user_longitude = radians(float(request.POST.get('longitude')))
+#         max_distance = 10  # 예시 거리 (단위: km)
 
+#         nearby_parks = []
 
+#         for park in Park.objects.all():
+#             park_latitude = radians(park.latitude)
+#             park_longitude = radians(park.longitude)
+
+#             # Haversine 공식을 사용하여 두 지점 간의 거리 계산
+#             dlon = park_longitude - user_longitude
+#             dlat = park_latitude - user_latitude
+#             a = sin(dlat/2)**2 + cos(user_latitude) * cos(park_latitude) * sin(dlon/2)**2
+#             c = 2 * atan2(sqrt(a), sqrt(1-a))
+#             distance = 6371 * c  # 지구의 반지름을 이용하여 거리 계산 (단위: km)
+
+#             if distance <= max_distance:
+#                 # 일정 거리 이하의 공원을 찾음
+#                 park_info = {
+#                     "name": park.name,
+#                     "address": park.add,
+#                     "latitude": park.latitude,
+#                     "longitude": park.longitude,
+#                 }
+#                 nearby_parks.append(park_info)
+
+#                 user_points, created = ParkVisitPoint.objects.create(user=request.user, park=park, earnedPoint=10, pointActivityDate=datetime.now())
+#                 user_points.earnedPoint = 10  # 10포인트 추가
+#                 user_points.pointActivityDate = datetime.now()
+#                 user_points.save()
+
+#                 user_profile, created = Profile.objects.get_or_create(user=request.user)
+#                 serializer = UserProfileSerializer(user_profile)
+
+#         return JsonResponse({"nearby_parks": nearby_parks})
