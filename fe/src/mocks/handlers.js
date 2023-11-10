be99a07c1a3829b2e12caaf0c9e0cebf2704c804
kye1115z <kye1115z@naver.com>
2023-11-11 03:46:48 +0900

    feat: Home msw 테스트

--- fe/src/mocks/handlers.js
+++ fe/src/mocks/handlers.js
@@ -1,9 +1,9 @@
 import { HttpResponse, http } from 'msw';
-import reviewDetail from './json/reviewDetail.json';
+import home from './json/home.json';
 
 // * test
 export const handlers = [
-  http.get(`/review/1/`, ({ request }) => {
-    return HttpResponse.json(reviewDetail);
+  http.get(`/`, ({ request }) => {
+    return HttpResponse.json(home);
   }),
 ];
