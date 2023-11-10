c1e36d08fe6a61f4c7314b4766f5b8eddecb766c
daeun han <daeunhan0723@gmail.com>
2023-11-10 19:42:10 +0900

    feat: price > user_points 일 때 예외 처리

--- be/donate/views.py
+++ be/donate/views.py
@@ -10,11 +10,14 @@ from .models import User
 from users.models import Profile
 
 
+from rest_framework.response import Response
+from rest_framework import status
+
+
 class DonatePostView(APIView):
     def post(self, request, pk):
         try:
             donation = Donation.objects.get(id=pk)  # 기부처의 id로 기부처정보를 가져옴
-            serializer = DonatePostSerializer(data=request.data)
         except Donation.DoesNotExist:
             return Response(
                 {"message": "해당 기부처를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND
@@ -22,8 +25,18 @@ class DonatePostView(APIView):
 
         serializer = DonatePostSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=self.request.user, donation=donation)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            # 사용자의 포인트가 충분한지 확인
+            user_points = self.request.user.profile.points
+            donation_amount = serializer.validated_data["price"]
+
+            if user_points >= donation_amount:
+                serializer.save(user=self.request.user, donation=donation)
+
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(
+                    {"message": "포인트가 부족합니다."}, status=status.HTTP_400_BAD_REQUEST
+                )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
