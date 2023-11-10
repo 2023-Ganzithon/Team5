02ac8a93026647c7f41c489e7ebe9df934ac72f6
kinsk2839 <kinsk2839@naver.com>
2023-11-10 17:24:00 +0900

    feat: 기부처 등록하는 요청을 처리하는 handler 추가

--- fe/src/mocks/handlers.js
+++ fe/src/mocks/handlers.js
@@ -6,4 +6,13 @@ export const handlers = [
   http.get('myPage/myPoint', () => {
     return HttpResponse.json(pointHistory);
   }),
+  http.post('myPage/donationRegister', async ({ request }) => {
+    const formData = await request.formData();
+    const name = formData.get('name');
+    const title = formData.get('title');
+    const comment = formData.get('comment');
+    const goal = formData.get('goal');
+
+    return HttpResponse.json({ name, title, comment, goal });
+  }),
 ];
