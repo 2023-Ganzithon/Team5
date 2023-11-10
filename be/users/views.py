12266caefcf2853a3e741a06a6a4dc5241e51ee1
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-11-10 21:53:26 +0900

    fix: profile update, read permission

--- be/users/views.py
+++ be/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 
@@ -24,3 +24,20 @@ class LoginView(generics.GenericAPIView):
 class ProfileView(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        # 현재 로그인한 유저의 프로필을 반환
+        return self.request.user.profile
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        # 현재 로그인한 유저와 프로필의 소유자가 일치하는지 확인
+        profile = self.get_object()
+        if profile.user.pk != self.request.user.pk:
+            return self.permission_denied(request)
+
+        # 소유자와 일치하면 프로필 업데이트 허용
+        return self.update(request, *args, **kwargs)
