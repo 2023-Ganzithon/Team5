9ece82e1d6c42778b4af6ab43865603dbe8c6cd3
kye1115z <kye1115z@naver.com>
2023-11-11 01:25:52 +0900

    feat: mallList 상수 적용

---
+++ fe/src/constants/mall.js
@@ -0,0 +1,50 @@
+import MAROCOROHO_BIG from '@assets/marcoroho.png';
+import MAROCOROHO_SMALL from '@assets/marcoroho_basic.png';
+import DAY30_BIG from '@assets/day30_BIG.png';
+import DAY30_SMALL from '@assets/day30_SMALL.png';
+import PLM_BIG from '@assets/PLM_BIG.png';
+import PLM_SMALL from '@assets/PLM_SMALL.png';
+import SAVE_CH_BIG from '@assets/SVC_BIG.png';
+import SAVE_CH_SMALL from '@assets/SVC_SMALL.png';
+import GFSTORE_BIG from '@assets/GF_BIG.png';
+import GFSTORE_SMALL from '@assets/GF_SMALL.png';
+
+export const MALLNAME = {
+  MARCOROHO: '마로코로호',
+  DAY30: '30일 상점',
+  PLM: '플리즈러브미',
+  SAVE_CH: '세이브더칠드런굿즈몰',
+  GFSTORE: '사람이 소중한 가게',
+};
+
+export const MALLPATH = {
+  MARCOROHO: 'https://marcoroho.com/',
+  DAY30: 'https://30daystore.com/home',
+  PLM: 'https://smartstore.naver.com/isyours',
+  SAVE_CH: 'https://savethegoods.marpple.shop/kr/',
+  GFSTORE: 'https://gfstore.org/',
+};
+
+export const MALL_BIGIMG = {
+  MARCOROHO: MAROCOROHO_BIG,
+  DAY30: DAY30_BIG,
+  PLM: PLM_BIG,
+  SAVE_CH: SAVE_CH_BIG,
+  GFSTORE: GFSTORE_BIG,
+};
+
+export const MALL_SMALLIMG = {
+  MARCOROHO: MAROCOROHO_SMALL,
+  DAY30: DAY30_SMALL,
+  PLM: PLM_SMALL,
+  SAVE_CH: SAVE_CH_SMALL,
+  GFSTORE: GFSTORE_SMALL,
+};
+
+export const MALL_DESC = {
+  MARCOROHO: '할머니에게 행복한 일자리,\n행복한 일상을 선물하다',
+  DAY30: '가치 있는 소비의 시작,\n유기동물과 함께 하는 세상',
+  PLM: '수익금의 일부를 유기동물 보호소에 기부하는 브랜드',
+  SAVE_CH: 'Save 더 칠드런,\n아이들의 미래를 지키다',
+  GFSTORE: '일상 속 소소하지만\n가치 있는 나눔, 위로, 기쁨',
+};
--- fe/src/constants/mallName.js
+++
@@ -1,3 +0,0 @@
-export const MALLNAME = {
-  MARCO: 'https://marcoroho.com/',
-};
