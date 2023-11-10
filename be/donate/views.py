838b54ea5751995eceae79a2cf1cd20a554b1832
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-11-10 14:24:11 +0900

    fix: 기부처 전체 조회

--- be/donate/views.py
+++ be/donate/views.py
@@ -8,26 +8,31 @@ from rest_framework.views import APIView
 from myPage.models import Donation, DonationPoint
 from users.models import Profile
 
+
 class DonatePostView(APIView):
     def post(self, request, pk):
         try:
             donation = Donation.objects.get(id=pk)  # 기부처의 id로 기부처정보를 가져옴
+            serializer = DonationSerializer(donation)
         except Donation.DoesNotExist:
-            return Response({'message': '해당 기부처를 찾을 수 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"message": "해당 기부처를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND
+            )
 
         serializer = DonatePostSerializer(data=request.data)
         if serializer.is_valid():
             profile = Profile.objects.get(user=self.request.user)
-            donation_point = serializer.validated_data.get('donation_point')
+            donation_point = serializer.validated_data.get("donation_point")
 
             # 사용자의 기부 포인트를 DonationPoint 모델에 기록
-            donation_point_record = DonationPoint(user=self.request.user, donation=donation, usedPoint=donation_point)
+            donation_point_record = DonationPoint(
+                user=self.request.user, donation=donation, usedPoint=donation_point
+            )
             donation_point_record.save()
 
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 
 # class DonatePostView(APIView):
