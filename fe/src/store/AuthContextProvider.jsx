04c9a0712a060005dc88e46764d3072e21478461
jwkim24 <kjw5841@naver.com>
2023-11-10 16:58:14 +0900

    Feat : AuthContextProvider 구현

---
+++ fe/src/store/AuthContextProvider.jsx
@@ -0,0 +1,66 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({
+    token:'',
+    user: {
+        nickname: '',
+        image:null,
+    },
+    loggedIn: false,
+    setToken: () => {},
+    setLoggedIn: () => {},
+    setUser: () => {},
+});
+
+export default AuthContext;
+
+
+export const AuthProvider = ({ children }) => {
+    const setToken = (newToken) => {
+        setState(prevState => (
+            {
+                ...prevState, 
+                token: newToken
+            }
+        ))
+    }
+    
+    const setLoggedIn = () => {
+        setState(prevState => (
+            {
+                ...prevState, 
+                loggedIn: !prevState.loggedIn
+            }
+        ))
+    }
+
+    const setUser = (nickname, image) => {
+        setState(prevState => ({
+            ...prevState,
+            user: {
+                nickname,
+                image,
+            },
+        }))
+    }
+
+    const initialState = {
+        token: '',
+        user: {
+            nickname: '',
+            image: null,
+        },
+        loggedIn: false,
+        setToken,
+        setLoggedIn,
+        setUser,
+    }
+
+    const [state, setState] = useState(initialState);
+
+    return (
+        <AuthContext.Provider value={state}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
