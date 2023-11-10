82ca2463fcc1b306eb9c439295192c8cc10a1f08
daeun han <daeunhan0723@gmail.com>
2023-11-10 20:50:00 +0900

    feat: donation detail에 user point 추가

--- be/donate/views.py
+++ be/donate/views.py
@@ -2,8 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Donation, Donate
-from .serializers import DonatePostSerializer
+from .serializers import DonatePostSerializer, ProfileSerializer
 from myPage.serializers import DonationSerializer
+from users.models import Profile
 
 
 class DonationReadView(APIView):
@@ -17,9 +18,26 @@ class DonateDetailView(APIView):
     def get(self, request, pk):
         try:
             donation = Donation.objects.get(id=pk)
-            serializer = DonationSerializer(donation)
+            donation_serializer = DonationSerializer(donation)
 
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            if self.request.user.is_authenticated:
+                user = self.request.user
+                profile = Profile.objects.get(user=user)
+
+                # 프로필 정보에서 포인트 정보만 가져와서 직렬화합니다.
+                profile_data = {"points": profile.points}
+
+                # 기부 정보와 프로필에서 가져온 포인트 정보를 합쳐 직렬화 데이터를 생성합니다.
+                serialized_data = {
+                    "donation": donation_serializer.data,
+                    "profile": profile_data,
+                }
+
+                return Response(serialized_data, status=status.HTTP_200_OK)
+            else:
+                return Response(
+                    {"message": "사용자가 인증되지 않았습니다."}, status=status.HTTP_401_UNAUTHORIZED
+                )
         except Donation.DoesNotExist:
             return Response(
                 {"message": "해당 기부처를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND
